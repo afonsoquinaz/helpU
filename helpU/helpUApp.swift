70d6aba12551b87456e3d566840375cc3ba51b6a
Afonso Quinaz <afonso.quinaz@gmail.com>
2022-03-25 19:32:13 +0000

    Initial Commit

---
+++ helpU/helpUApp.swift
@@ -0,0 +1,17 @@
+//
+//  helpUApp.swift
+//  helpU
+//
+//  Created by Afonso Quinaz on 25/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct helpUApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
